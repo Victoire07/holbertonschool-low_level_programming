4a09bc22f34861732d42b6c3d9cf5fa7dfe13b03
Victoire07 <victoireboutin07@gmail.com>
2025-02-17 16:36:11 +0000

    programme C qui imprime exactement 'blabla' suivi d1 nvl ligne

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Print a string : afficher une string 
+ * donc chaine de caractere donc plus de un cara
+ * Return: Always 0 : toujours 0
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
 
