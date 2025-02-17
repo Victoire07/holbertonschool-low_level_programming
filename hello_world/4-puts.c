3edbf154fc1d81ecea94d16f2a595c464d31b004
Victoire07 <victoireboutin07@gmail.com>
2025-02-17 16:40:29 +0000

    Fix task 4: betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Print a string : afficher une string 
+ * main - Print a string : afficher une string
  * donc chaine de caractere donc plus de un cara
  * Return: Always 0 : toujours 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
