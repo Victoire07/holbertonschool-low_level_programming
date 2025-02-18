b8cbd1919077ded9f59d35d72d8698e8a2b5e54f
Victoire07 <victoireboutin07@gmail.com>
2025-02-18 09:46:02 +0000

    Programme C imprime '...' et nvl ligne

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-* main – Programme C qui imprime et nvl lign
+* main -  Programme C qui imprime et nvl lign
+* Return: 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
