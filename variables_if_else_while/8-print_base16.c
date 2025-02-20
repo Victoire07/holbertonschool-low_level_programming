c7d8670341c7dd3a942120d70528e5cb1499f5a1
Your Victoire07 <victoireboutin07@gmail.com>
2025-02-20 10:42:01 +0000

    8 num et lett

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 	char hex;
 	int num;
 
-	for (hex = 'a'; hex <= 'f'; hex++)
-	{
-		putchar(hex);
-	}
-
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
+	}
+
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
 	}
 		putchar('\n');
 	return (0);
