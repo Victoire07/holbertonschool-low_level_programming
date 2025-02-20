f313ef139ab1704bbf5436fbcfae82a71d7accdb
Your Victoire07 <victoireboutin07@gmail.com>
2025-02-20 16:49:55 +0000

    Exo 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - Prints an alphabets
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	char min;
+
+	for (min = 'a'; min <= 'z'; min++)
+		_putchar(min);
+
+	_putchar('\n');
+}
