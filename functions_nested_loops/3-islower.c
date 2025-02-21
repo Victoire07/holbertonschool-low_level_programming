177e4b45a06ac03441b3e172a00f062de2aae0f8
Victoire07 <victoireboutin07@gmail.com>
2025-02-21 10:49:37 +0000

    Exo 3 fonction presen caract minuscule

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
-* islower - Prints a function that checks for lowercase character
-* @c: The character to be checked
-*
-* Return 1 if c is lowercase
-* Return 0 otherwise
-*/
-
+ * _islower - Checks for a lowercase character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
