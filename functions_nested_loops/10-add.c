483b556b7e1df892c7d4da6862888acdc6fca854
Victoire07 <victoireboutin07@gmail.com>
2025-02-23 19:43:43 +0000

    Task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *add - Print result
+ *
+ *@num1: first integers.
+ *
+ *@num2: second integers.
+ * Return: the results
+ */
+
+int add(int num1, int num2)
+{
+	int res;
+
+	res = num1 + num2;
+	{
+	return (res);
+	}
+}
