8ac289fe40dc5831fca74b505c28facfb2add2a3
Victoire07 <victoireboutin07@gmail.com>
2025-02-24 10:19:55 +0000

    Multiplier avec return 2 entiers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Print a function that multiplies two integers
+ * @a: 1st integers
+ * @b: 2nd integers
+ *
+ * Return: The total of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
