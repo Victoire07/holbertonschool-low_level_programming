2a9d5cdfd9f397122a86d5decf7e7c1835707c6e
Victoire07 <victoireboutin07@gmail.com>
2025-02-26 09:06:59 +0000

    Écrivez une fonction qui imprime un carré, suivi d'une nouvelle ligne.

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *print_square - Write a function that prints a square, followed by a new lne
+ * print_square - Write a function that prints a square, followed by a new lne
  * @size: The size of the square
  *
- * Description : If size is 0 or less, the function should print only a new lne
+ * Description: If size is 0 or less, the function should print only a new lne
  */
 
 void print_square(int size)
@@ -11,7 +11,7 @@ void print_square(int size)
 	int l, c;
 
 	if (size <= 0)
-	{	
+	{
 		_putchar('\n');
 		return;
 	}
