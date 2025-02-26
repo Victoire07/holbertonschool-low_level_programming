d2f75e0d7dc06f0b5c3489ea29fa176c8084e3d9
Victoire07 <victoireboutin07@gmail.com>
2025-02-26 15:54:01 +0000

    EXO EN COURS PAS FINI

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - Write a fnctin that prints a triangle followed by a new lne
+ * @size: The size of the triangle
+ *
+ * Description: If size is 0 or less, the function should print only a new line
+ */
+
+void print_triangle(int size)
+{
+	int l, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (l = 0; l < size; l++)
+	{
+		for (c = 0; c < size; c++)
+		{
+			_putchar('#');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
