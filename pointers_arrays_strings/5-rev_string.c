e48bd738c688cc673d3dc9a5ca07638f64b9355f
Victoire07 <victoireboutin07@gmail.com>
2025-03-01 17:00:46 +0000

    Write a function that reverses a string.

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,28 @@
 #include "main.h"
+/**
+ * rev_string - Fonction qui inverse une chaine de caracteres
+ * @s: Pointeur vers la chai®ne de cars Ã  inver
+ *
+ * Cette fonction modifie la chaÃ®ne en place sans utilise e©moire suppe©meni
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1;
+
+	for (i = 0; i < j; j--)
+	{
+		char temp = s[i];
+
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+	}
+}
