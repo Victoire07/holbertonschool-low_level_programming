80f816fc7d19d4c5bfb52492270d03fb73539546
Victoire07 <victoireboutin07@gmail.com>
2025-03-02 16:08:09 +0000

    Task

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_half - fonction qui imprime la moitie© d'unechaine de caraere + \n
+ * @str: Pointeur vers la chaine de caracteres a afficher
+ *
+ */
+void puts_half(char *str)
+{
+	int length = 0, start;
+	while (str[length] != '\0')
+	length++;
+	start = (length + 1) / 2;
+	while (str[start] != '\0')
+	{
+	    _putchar(str[start]);
+	start++;
+	}
+}
