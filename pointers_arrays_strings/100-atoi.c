e1967d308b8862f87be1a2cf3e930901733ffe84
Victoire07 <victoireboutin07@gmail.com>
2025-03-03 16:08:06 +0000

    fonction qui convertit une chaîne de caractères en un nombre entier

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,4 +9,27 @@
  */
 int _atoi(char *s)
 {
+	int sign = 1; /* Signe du nombre (positif par defaut!) */
+	int num = 0;
+
+	if (s == NULL) /* ici if verifie si la chaine est nul donc NULL */
+	return (0);
+
+	while (*s == ' ') /* permet dignorer les espaces dou le ' ' */
+	s++;
+
+	while (*s == '+' || *s == '-') /* Les signes + et - */
+	{
+	if (*s == '-')
+	sign *= -1; /* permet de inverser les signes si ya le - */
+	s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		num = (num * 10) + (*s - '0');
+		s++;
+	}
+
+	return (num * sign);
 }
