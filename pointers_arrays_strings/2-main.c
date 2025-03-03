7b5fa891fabcf2098dcde37acf34e6d51676e55f
Victoire07 <victoireboutin07@gmail.com>
2025-03-03 14:52:31 +0000

    Test a voir

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -1,11 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+/*
 int main(void)
 {
     char *str;
@@ -14,5 +10,25 @@ int main(void)
     str = "My first strlen!";
     len = _strlen(str);
     printf("%d\n", len);
+    return (0);
+}
+*/
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
     return (0);
 }
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Trouve la premiere occurrence d1 cara dans une chaine
+ * @s: La chaine de caracteres a parcourir
+ * @c: Le caractere a chercher
+ *
+ * Return: Un pointeur vers la premiere occurrence de `c` dans `s`,
+ *         ou NULL si `c` n'est pas present ca retourne ca dcp
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+	if (*s == c)
+	return (s);
+	s++;
+	}
+
+	if (*s == c)
+	return (s);
+
+	return (NULL);
+}
