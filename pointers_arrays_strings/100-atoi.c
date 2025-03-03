ba725b8561be460faf09721b4d36a1c92ef124ef
Victoire07 <victoireboutin07@gmail.com>
2025-03-03 15:37:27 +0000

    ....

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _atoi - Convertit une chaine en entier
+ * @s: La chaine a convertir
+ *
+ * Return: L'entier extrait de la chaine, ou 0 si aucun chiffre nest prsnt
+ */
+int _atoi(char *s)
+{
+}
