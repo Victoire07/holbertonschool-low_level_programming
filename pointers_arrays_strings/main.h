188bfb71a02aefc3680d545860eee0eb26bf0acc
Victoire07 <victoireboutin07@gmail.com>
2025-03-05 13:36:22 +0000

    Code bien compris : fonction qui imprime 1 echiquier

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -23,5 +25,6 @@ char *_strstr(char *haystack, char *needle);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 
 #endif
