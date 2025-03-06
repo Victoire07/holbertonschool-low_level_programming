ff3bad794467eca4a44ae46bf28ebf1066b8bba5
Victoire07 <victoireboutin07@gmail.com>
2025-03-06 09:12:42 +0000

    Recursion, fonction qui imprime string suivi nouvelle ligne

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _puts_recursion - Prints a string followed by a new line
- * @s: String to print
- *
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: String to print.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
-		putchar(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 }
--- recursion/main.h
+++ recursion/main.h
@@ -2,6 +2,8 @@
 #define MYHEADER_H
 #include <stdio.h>
 #include <stdlib.h>
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 
 #endif
