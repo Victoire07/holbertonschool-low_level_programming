4bc265c70ab15467ea72113535b97464f6348f49
Victoire07 <victoireboutin07@gmail.com>
2025-03-07 16:51:32 +0000

    programme qui imprime tous les arguments qu'il reçoit

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -7,10 +7,14 @@
  *
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
