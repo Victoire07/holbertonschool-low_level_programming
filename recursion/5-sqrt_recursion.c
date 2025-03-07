df8a30414129a032030c505d30bd980b03cebe6e
Victoire Boutin <victoireboutin@MacBook-Air-de-Victoire.local>
2025-03-07 14:33:43 +0100

    Fonction qui retourne la racine carree naturelle dun nombre

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,28 +8,35 @@
 
 int _sqrt_recursion(int n)
 {
-    if (n <= 0)
-    {
-        return (-1);
-    }
-    return (square_root(n, 0));
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	return (square_root(n, 0));
+
 }
+/**
+ * square_root - finds the square root
+ *
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: 1
+ */
+
 int square_root(int x, int y)
 {
-    if (y * y > x)
-    {
-        return (-1);
-    }
-    else if (y * y == x)
-    {
-        return (y);
-    }
-    else
-    {
-        return (square_root(x, y + 1));
-    }
-    return (1);
+	if (y * y > x)
+	{
+		return (-1);
+	}
+	else if (y * y == x)
+	{
+		return (y);
+	}
+	else
+	{
+		return (square_root(x, y + 1));
+	}
+	return (1);
 }
-
-
-
