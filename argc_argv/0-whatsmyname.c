83e29326dd62323fe93725e03d8dbf4aee90664f
Victoire07 <victoireboutin07@gmail.com>
2025-03-07 15:10:20 +0000

    programme qui imprime son nom, suivi d'une nouvelle ligne

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  Programme qui imprime son nom, suivi d'une nouvelle ligne
+ * @argc: argument count (nombre)
+ * @argv: argument vector (unused) (vecteur)
+ *
+ * Return: nbr d'arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	 printf("%s\\n", argv[0]);
+	  return (0);
+
+}
