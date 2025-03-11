b13765d3239a377722529eabb85e7b1617d093e5
Victoire07 <victoireboutin07@gmail.com>
2025-03-11 16:31:13 +0100

    fonction qui renvoie un pointeur vers un espace nouvellement alloué en mémoire, qui contient une copie de la chaîne de caractères donnée en paramètre

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * str_concat - Creer une fonction qui alloue et retourne 1copie d'une chaine
+ * @s1: 1ere chaine
+ * @s2: 2eme chaine
+ * Return: Pointeur vers une nouvelle chaîne contenant s1 suivi de s2,
+ * NULL si l'allocation échoue.
+ * Traite NULL comme une chaîne vide.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+    char *concat;
+    int i, j, len1 = 0, len2 = 0;
+
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1]; len1++)
+		;
+	}
+
+	if (s2 != NULL)
+	{
+		for (len2 = 0; s2[len2]; len2++)
+		;
+	}
+
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+	return (NULL);
+
+	for (i = 0; i < len1; i++)
+	concat[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+	concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
+}
+
