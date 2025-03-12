1cb82051867835c6d20e5fb5f40f5f763356545b
Victoire07 <victoireboutin07@gmail.com>
2025-03-12 14:51:47 +0100

    fonction qui renvoie un pointeur vers un tableau d'entiers à 2 dimensions

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,1 +1,45 @@
 #include "main.h"
+
+/**
+ * alloc_grid - Fonction qui renvoie un ptr vers un tableau d'entiers a 2 dims
+ * @width: Largeur de la grille
+ * @height: Hauteur de la grille
+ * Return: Un pointeur vers la grille allouée ou NULL en cas d'echec
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int y;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	array = malloc(height * sizeof(int *));
+	if (!array)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (!array[i])
+		{
+			for (y = 0; y < i; y++)
+			{
+				free(array[y]);
+			}
+			free(array);
+			return (NULL);
+		}
+
+		for (y = 0; y < width; y++)
+		{
+			array[i][y] = 0;
+		}
+	}
+	return (array);
+}
