401878acb3a903c74471228b19fbbb04664142d7
Victoire07 <victoireboutin07@gmail.com>
2025-03-13 14:43:44 +0100

    Définir un nouveau type struct dog avec certains elements : nom, age et proprietaire

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure qui va definir des informations sur le chien
+ * @name: le nom du chien
+ * @age: l'age du chien
+ * @owner: le proprietaire du chien
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
