41790980faba8a6b938cdaf6497e80ccd231a57c
Victoire07 <victoireboutin07@gmail.com>
2025-03-14 14:13:15 +0100

    Liberer les chiens (free)

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - fonction qui libere les chiens
+ * @d: structure a liberer
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	return;
+
+	free(d->name);
+
+	free(d);
+}
