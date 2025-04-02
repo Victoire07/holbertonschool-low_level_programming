dd41ac7e26029f272c0f391663e3c4c0d4e9f0e5
Victoire07 <victoireboutin07@gmail.com>
2025-04-02 18:00:58 +0200

    2eme  test exo 2 : fnctn qui ajte 1 nveau nœud au début d'1 lst de dlistint_t

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
+	if (*head != NULL)
+	return (NULL);
+
 
 	(*head)->prev = new_node;
 	*head = new_node;
