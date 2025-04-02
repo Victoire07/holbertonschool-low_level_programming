8b239910446cd2b368ae55904eafaeed9cb7d72d
Victoire07 <victoireboutin07@gmail.com>
2025-04-02 22:45:39 +0200

    test q°3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,29 @@
  * Return:  l'adresse du nouvel element, ou NULL en cas d'echec
  */
 
- dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
- {
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
- }
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	*head = new_node;
+
+	dlistint_t *last = *head;
+
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new_node;
+	new_node->prev = last;
+
+
+}
