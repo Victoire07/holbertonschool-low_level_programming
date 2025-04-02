ec577755e76bcb5c1584aabe711a735f0d6368f4
Victoire07 <victoireboutin07@gmail.com>
2025-04-02 17:53:22 +0200

    Premier test exo 2 : fnctn qui ajte 1 nveau nœud au début d'1 lst de dlistint_t

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - fnctn qui ajte 1 nveau nœud au début d'1 lst de dlistint_t
+ * @head: Liste
+ * @n: element a ajouter
+ * Return: l'adresse du nouvel element, ou NULL en cas d'echec
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	(*head)->prev = new_node;
+	*head = new_node;
+
+	return (new_node);
+}
