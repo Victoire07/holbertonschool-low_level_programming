47fd9c31287cfc948794098e8d780d251da535d7
Victoire07 <victoireboutin07@gmail.com>
2025-04-02 02:11:54 +0200

    fonction qui libere une liste list_t

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - ecrire une fonction qui libere une liste list_t
+ * @head: liste
+ */
+
+ void free_list(list_t *head)
+ {
+	list_t *temporaire;
+
+	while (head != NULL)
+	{
+		temporaire = head->next;
+		free(head->str);
+		free(head);
+		head = temporaire;
+	}
+	
+ }
