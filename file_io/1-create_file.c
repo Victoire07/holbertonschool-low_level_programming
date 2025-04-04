222fb2f74d50e9bef3baafd597d1ffdd02796587
Victoire07 <victoireboutin07@gmail.com>
2025-04-04 20:18:06 +0200

    Q1: Sauvegarde du prototype dans main.h et création du fichier 1 avec une norme betty et le début de la fonction

---
+++ file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_file - Créer une fonction qui crée un fichier
+ * @filename: nom de fichier
+ * @text_content: texte à écrire
+ * Return: 1 en cas de succès, -1 en cas d'échec
+ */
+
+ int create_file(const char *filename, char *text_content)
+ {
+
+ }
