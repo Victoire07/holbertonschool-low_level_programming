e21a0df7c31a3479f0fbc55d246eca2362c2d7c2
Victoire07 <victoireboutin07@gmail.com>
2025-04-04 14:24:23 +0200

    Q0 : sauvegarde du commentaire Betty et prototype de fonction

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * read_textfile - lit un fich texte et l'imprime sur la sortie standard POSIX
+ * @filename: Nom fichier
+ * @letters: Le nombre de lettres à lire et à imprimer
+ * Return: le nombre de lettres à lire et à impirmer
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
