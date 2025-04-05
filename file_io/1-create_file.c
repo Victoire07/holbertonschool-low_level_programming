253894f710ffdf57ad134179fb721f8902cdab8f
Victoire07 <victoireboutin07@gmail.com>
2025-04-05 19:10:22 +0200

    Q1 : Non terminé pour l'instant vérifie si fichier NULL si NULL return -1 pour erreur et créer les permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,7 +7,23 @@
  * Return: 1 en cas de succès, -1 en cas d'échec
  */
 
- int create_file(const char *filename, char *text_content)
- {
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
 
- }
+	if (filename == NULL)
+	return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	close(fd);
+	return (1);
+}
