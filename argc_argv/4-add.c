2523131b669f82746c570f06b5326aa9d8c510b1
Victoire07 <victoireboutin07@gmail.com>
2025-04-07 05:25:39 +0200

    Modification avec ajout des conditions

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,10 +10,35 @@ int main(int argc, char *argv[])
 {
 	int somme = 0;
 	int i;
+	int is_valid;
+	int j;
 
 	if (argc == 1)
-	printf("0\n");
-	return(0);
-
-	for (i= 1; i <= argc; i++)
+	{
+		printf("0\n");
+		return(0);
+	}
+	else
+	{
+		for (i= 1; i < argc; i++)
+		{
+			is_valid = 1;
+		}
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				is_valid = 0;
+				break;
+			}
+    	}
+		if (is_valid) 
+		{
+			atoi(argv[i])
+		}
+		else 
+		{
+			printf("Error\n");
+			return (1);
+    	}
 }
