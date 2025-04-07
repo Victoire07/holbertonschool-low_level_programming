3d1152d1a4f2c4057d9dc6708a30c69822d70f99
Victoire07 <victoireboutin07@gmail.com>
2025-04-07 05:12:11 +0200

    Q4 : Premier jet quetsion 4 sans la boucle for

---
+++ argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Programme qui additionne des nombres positifs
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int somme = 0;
+	int i;
+
+	if (argc == 1)
+	printf("0\n");
+	return(0);
+
+	for (i= 1; i <= argc; i++)
+}
