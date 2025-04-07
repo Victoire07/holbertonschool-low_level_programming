72e940344ffa39ecbc323559e3e6eed947aa746b
Victoire07 <victoireboutin07@gmail.com>
2025-04-07 05:34:00 +0200

    Ajout somme, affichage erreur et somme

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Programme qui additionne des nombres positifs
  * @argc: argument count
@@ -18,27 +19,36 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return(0);
 	}
-	else
+	
+	for (i= 1; i < argc; i++)
 	{
-		for (i= 1; i < argc; i++)
+		is_valid = 1;
+	}
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (argv[i][j] < '0' || argv[i][j] > '9')
 		{
-			is_valid = 1;
+			is_valid = 0;
+			break;
 		}
-		for (int j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				is_valid = 0;
-				break;
-			}
-    	}
+    }
 		if (is_valid) 
 		{
-			atoi(argv[i])
+			somme += atoi(argv[i])
 		}
 		else 
 		{
 			printf("Error\n");
 			return (1);
     	}
+		printf("%d\n", somme);
+		return 0;
+	
 }
+
+
+
+
+        
+
+  
